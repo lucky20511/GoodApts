0e393a4c693195ebb3f72db1c2b1e06df4399e63
HsunFu Liu <lucky20511@gmail.com>
2017-10-30 18:55:06 -0700

    Render the result JSON from back-end to chartResult.html

--- server/routes/router.js
+++ server/routes/router.js
@@ -13,10 +13,33 @@ const mongoose = require('mongoose');
 
 //Root Index
 router.get('/', function (req, res) {
-	res.render('index');
+	res.render('di/index');
 });
 
+router.get('/result', function (req, res) {
+	result = {
+		"name":"test",
+		"price":1000000
+	}
+	res.render('result', {
+		result: result
+	});
+});
 
+router.post('/predict', function (req, res) {
+	var result_bed = [{"bedrooms":1,"prices":93.24},
+              {"bedrooms":2,"prices":95.35},
+              {"bedrooms":3,"prices":98.84},
+              {"bedrooms":4,"prices":99.92},
+              {"bedrooms":5,"prices":99.8},
+              {"bedrooms":6,"prices":99.47},
+              {"bedrooms":7,"prices":100.39},
+              {"bedrooms":8,"prices":100.4},
+              {"bedrooms":9,"prices":100.81},
+              {"bedrooms":10,"prices":103.92}];
+	res.render('di/chartResult', {result_bed:result_bed});
+
+});
 
 
 
@@ -25,6 +48,21 @@ router.get('/', function (req, res) {
 /***************************/
 
 //TBD
+router.get('/predict/DNN', function (req, res) {
+
+});
+
+router.get('/predict/SVR', function (req, res) {
+
+});
+
+router.get('/predict/RDF', function (req, res) {
+
+});
+
+router.get('/predict/BAY', function (req, res) {
+
+});
 
 
 module.exports = router;
