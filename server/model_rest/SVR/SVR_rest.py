2758946e0d3d2e833073e6d765c35a53ddee0300
diBao <starrynicol@hotmail.com>
2017-11-02 04:21:32 -0700

    SVR

---
+++ server/model_rest/SVR/SVR_rest.py
@@ -0,0 +1,36 @@
+import pickle
+import pandas as pd
+from flask import Flask, jsonify, json, request
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello SVR!"
+
+@app.route('/SVR', methods = ['POST'])
+def svr():
+    if request.method == 'POST':
+        req_post = json.loads(request.data)
+
+        lat = req_post['lat']
+        lng = req_post['lng']
+        neigh = req_post['neighborhood']
+        lot_sqft = req_post['lot_square_feet']
+        year_built = req_post['year_built']
+        sqft = req_post['square_feet']
+        rooms = req_post['rooms']
+        beds = req_post['bedrooms']
+        bathes = req_post['bathrooms']
+        median = req_post['median_per_sqft']
+
+        x = pd.DataFrame([[lat, lng, neigh, lot_sqft, 0, year_built, sqft, rooms, beds, bathes, median]])
+
+        # load model
+        filename = 'svr_model.sav'
+        svr_model = pickle.load(open(filename, 'rb'))
+        price = svr_model.predict(x)
+
+        return jsonify(price=int(price[0])), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10001, debug=True)
---
+++ server/model_rest/readme.md
@@ -0,0 +1,35 @@
+## url, body & response
+
+### SVR
+**POST localhost:10001/SVR**
+```
+{
+  "lat": 37.297955,
+  "lng": -121.812944,
+  "neighborhood": 17,
+  "lot_square_feet": 8140,
+  "year_built": 1972,
+  "square_feet": 1799,
+  "rooms": 7.0,
+  "bedrooms": 4.0,
+  "bathrooms": 1.5,
+  "median_per_sqft": 455
+}
+```
+
+response:
+```
+{
+  "price": 702263
+}
+```
+
+- no property class in the post body, default to 1 in SVR_rest.py
+
+-----------
+
+### DNN
+
+### Random Forest
+
+### Bayesian
