eb559c593fd0f6a9a388b8ed66f13051e1544e87
GCystem <georgey.chen36@gmail.com>
2017-11-05 18:55:33 -0800

    update bay

--- Predictors/Bayesian/bay_rest.py
+++ Predictors/Bayesian/bay_rest.py
@@ -3,6 +3,7 @@ import pickle
 from flask import Flask, request, Response, json
 from flask import jsonify
 
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 app = Flask(__name__)
 
 @app.route('/Bay', methods = ['POST'])
@@ -10,19 +11,18 @@ def train():
     p_body = json.loads(request.data)
     lat = float(p_body["lat"])
     lng = float(p_body["lng"])
-    neighborhood = float(p_body["neighborhood"])
-    lot_square_feet = float(p_body['lot_square_feet'])
+    property_class = float(p_body["property_class"])
     year_built = float(p_body['year_built'])
     square_feet = float(p_body['square_feet'])
-    rooms = float(p_body['rooms'])
     bedrooms = float(p_body['bedrooms'])
     bathrooms = float(p_body['bathrooms'])
-    median_per_sqft = float(p_body['median_per_sqft'])
-    
-    loaded_model = pickle.load(open('trained_model.sav', 'rb'))
-    res = loaded_model.predict([[lat, lng, neighborhood, lot_square_feet, year_built, square_feet, rooms, bedrooms, bathrooms, median_per_sqft]])
+    zip_code = float(p_body['zip_code'])   
+    transaction_year = float(p_body['transaction_year'])  
+
+    res = loaded_model.predict([[lat, lng, property_class, year_built, square_feet, bedrooms, bathrooms, zip_code, transaction_year]])
     return jsonify(price = res[0])
 
 if __name__ == '__main__':
+
 	app.run(host='0.0.0.0', port=10002, debug=True)
 	
