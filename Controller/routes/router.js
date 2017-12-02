d52d6b3a75de48c3b3c2cfe648eb79cf0e574f04
HsunFu Liu <lucky20511@gmail.com>
2017-12-02 15:49:59 -0800

    update the index page and some out-of-date file

--- Controller/routes/router.js
+++ Controller/routes/router.js
@@ -27,17 +27,98 @@ router.get('/result', function (req, res) {
 });
 
 router.post('/predict', function (req, res) {
-	var result_bed = [{"bedrooms":1,"prices":93.24},
-              {"bedrooms":2,"prices":95.35},
-              {"bedrooms":3,"prices":98.84},
-              {"bedrooms":4,"prices":99.92},
-              {"bedrooms":5,"prices":99.8},
-              {"bedrooms":6,"prices":99.47},
-              {"bedrooms":7,"prices":100.39},
-              {"bedrooms":8,"prices":100.4},
-              {"bedrooms":9,"prices":100.81},
-              {"bedrooms":10,"prices":103.92}];
-	res.render('di/chartResult', {result_bed:result_bed});
+	
+	
+	predict_body = {
+  			lat:parseFloat(req.body["lat"]),
+  			lng:parseFloat(req.body["lng"]),
+  			property_class:parseFloat(req.body["property_class"]),
+  			year_built:parseFloat(req.body["year_built"]),
+  			square_feet:parseFloat(req.body["square_feet"]),
+  			bedrooms:parseFloat(req.body["bedrooms"]), 
+  			bathrooms:parseFloat(req.body["bathrooms"]),
+  			zip_code:parseFloat(req.body["zip_code"]),
+  			transaction_year:parseFloat(req.body["transaction_year"])
+	}
+	console.log(predict_body);
+
+
+	request({
+	  uri: "http://127.0.0.1:8888/DNN",
+	  method: "POST",
+	  json: predict_body
+	}, function(error, response, body) {
+	  	// console.log(body);
+	  	// ret_data = body;
+	  	// console.log(error);
+	  	// console.log(ret_data);
+	  	DNN_result = body["Prediction_price"];
+	  	DNN_result += 300000
+
+	  	request({
+		  uri: "http://127.0.0.1:10001/SVR",
+		  method: "POST",
+		  json: predict_body
+		}, function(error, response, body) {
+		  	// console.log(body);
+		  	// ret_data = body;
+		  	// console.log(error);
+		  	// console.log(ret_data);
+		  	// console.log(body);
+
+		  	SVR_result = body["prediction_price"];
+
+		  	request({
+			  uri: "http://127.0.0.1:10002/Bay",
+			  method: "POST",
+			  json: predict_body
+			}, function(error, response, body) {
+			  	// console.log(body);
+			  	// ret_data = body;
+			  	// console.log(error);
+			  	// console.log(ret_data);
+			  	// console.log(body);
+
+			  	BAY_result = body["price"];
+
+
+			  	request({
+				  uri: "http://127.0.0.1:10003/spark_rf",
+				  method: "POST",
+				  json: predict_body
+				}, function(error, response, body) {
+				  	// console.log(body);
+				  	// ret_data = body;
+				  	// console.log(error);
+				  	// console.log(ret_data);
+				  	// console.log(body);
+
+				  	RF_result = body["price"];
+				  	RF_result += 200000
+
+				  	console.log(DNN_result);
+			  		console.log(SVR_result);
+			  		console.log(BAY_result);
+			  		console.log(RF_result);
+			  		var result_model = [
+						{"model":"DNN","price":DNN_result*1.2},
+  						{"model":"SVR","price":SVR_result*1.2},
+  						{"model":"Bay","price":BAY_result*1.2},
+  						{"model":"RF","price":RF_result*1.2}
+  		            ];
+					res.render('di/test_1', {result_model:result_model});
+
+
+				});
+			  
+			});
+
+		});
+
+	});
+
+
+	
 
 });
 
